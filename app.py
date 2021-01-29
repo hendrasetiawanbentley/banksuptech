64ea78208634521489aaf081119f2096df41235a
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2021-01-29 11:53:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,59 +27,45 @@ server = app.server
 app.config['suppress_callback_exceptions']=True
 
 app.layout = html.Div([
-    dcc.Tabs(
-        id="tabs-with-classes",
-        value='tab-2',
-        parent_className='custom-tabs',
-        className='custom-tabs-container',
-        children=[
-            dcc.Tab(
-                label='Tab one',
-                value='tab-1',
-                className='custom-tab',
-                selected_className='custom-tab--selected'
-            ),
-            dcc.Tab(
-                label='Tab two',
-                value='tab-2',
-                className='custom-tab',
-                selected_className='custom-tab--selected'
-            ),
-            dcc.Tab(
-                label='Tab three, multiline',
-                value='tab-3', className='custom-tab',
-                selected_className='custom-tab--selected'
-            ),
-            dcc.Tab(
-                label='Tab four',
-                value='tab-4',
-                className='custom-tab',
-                selected_className='custom-tab--selected'
-            ),
+    dcc.Tabs([
+        dcc.Tab(label='Tab one', children=[
+            dcc.Graph(
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [4, 1, 2],
+                            'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [2, 4, 5],
+                         'type': 'bar', 'name': u'Montréal'},
+                    ]
+                }
+            )
         ]),
-    html.Div(id='tabs-content-classes')
+        dcc.Tab(label='Tab two', children=[
+            dcc.Graph(
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [1, 4, 1],
+                            'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [1, 2, 3],
+                         'type': 'bar', 'name': u'Montréal'},
+                    ]
+                }
+            )
+        ]),
+        dcc.Tab(label='Tab three', children=[
+            dcc.Graph(
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [2, 4, 3],
+                            'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [5, 4, 3],
+                         'type': 'bar', 'name': u'Montréal'},
+                    ]
+                }
+            )
+        ]),
+    ])
 ])
-
-
-@app.callback(Output('tabs-content-classes', 'children'),
-              Input('tabs-with-classes', 'value'))
-def render_content(tab):
-    if tab == 'tab-1':
-        return html.Div([
-            html.H3('Tab content 1')
-        ])
-    elif tab == 'tab-2':
-        return html.Div([
-            html.H3('Tab content 2')
-        ])
-    elif tab == 'tab-3':
-        return html.Div([
-            html.H3('Tab content 3')
-        ])
-    elif tab == 'tab-4':
-        return html.Div([
-            html.H3('Tab content 4')
-        ])
 
 
 if __name__ == '__main__':
