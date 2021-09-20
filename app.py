b081685fd62871a06b9c066ba67cf18940038bd7
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2021-09-20 15:08:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ df = pd.read_csv(data_url)
 tempo=pd.read_csv('new_tempodotco_tweets.csv')
 tempo['signal']=tempo.text.str.contains('OJK')
 tempoclean=tempo.loc[tempo.signal==True,:]
-
+df = tempoclean
 
 def clean_tweet(tweet):
     return ' '.join(re.sub('(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', tweet).split())#The long string of what seems like nonsense essentially takes away any characters that aren’t normal english words/letters.
@@ -62,7 +62,7 @@ def analyze_sentiment(tweet):
         return 'Negative'
     
 tempoclean["Sentiment"] = tempoclean['cleantweet'].apply(lambda x: analyze_sentiment(x))
-df = tempoclean
+
 
 app.layout = html.Div([
      html.Div([html.H1('Financial Services Supervisory Technology', style={'textAlign': 'center','background': '#f9f9f9','box-shadow': '0 0 1px rgba(0,0,0,.2), 0 2px 4px rgba(0,0,0,.1)','border-radius': '5px','margin-bottom': '20px','text-shadow': '1px 1px 1px rgba(0,0,0,.1)'})]),
