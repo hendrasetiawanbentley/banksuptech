23dbc82b32844e6eb08f0a7531d2dc76dc7ab4e0
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2022-03-11 09:46:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -197,20 +197,16 @@ app.layout = html.Div([
            html.P('Terdapat dua jenis metode tranformasi yang digunakan di OJK Coin v2.0 yaitu CMAX/CMIN(60)1 dan Δ20d2. Kedua metode tersebut kemudian dibandingkan untuk mencari metode tranformasi terbaik yang mampu meningkatkan akurasi OJK Coin. Metode tranformasi terpilih akan diaplikasikan kepada seluruh indikator yang ada di OJK Coin secara seragam kecuali segmen IJK. Metode CMAX/CMIN(60) dipilih sebagai metode transformasi tunggal di dalam OJK Coin v3.0.'),
            html.P('Indikator volatilitas IHSG ditambahkan menjadi salah satu indikator segmen pasar saham sebagai alat pengukuran tekanan volatilitas pergerakan harga indeks. Volatilitas IHSG dihitung secara historis dalam rolling rentang sampel 1 bulan. Indikator volatilitas IHSG ini kemudian ditranformasi menggunakan metode CMIN(60), seragam dengan indikator-indikator lainnya.'),
            html.Br(), 
-           html.P('OJK Coin telah disempurnakan lebih lanjut dengan cara: 1. Mengganti indikator segmen pasar utang dan nilai tukar menjadi  CMIN(60) data last price; dan 2. Menambahkan indikator CMIN(60) volatilitas 1M daily return IHSG di segmen pasar saham.'), 
+           html.P('OJK Coin telah disempurnakan lebih lanjut dengan cara: 1. Mengganti indikator segmen pasar utang dan nilai tukar menjadi  CMIN(60) data last price; dan 2. Menambahkan indikator CMIN(60) volatilitas 1M daily return IHSG di segmen pasar saham.'),
+           html.P('1. Mengganti indikator segmen pasar utang dan nilai tukar menjadi  CMIN(60) data last price;'),
+           html.P('2. Menambahkan indikator CMIN(60) volatilitas 1M daily return IHSG di segmen pasar saham.'),
            html.Br(),
-           html.H3('Design OJK Coincidence Index 3.0'),
-           html.Img(src=app.get_asset_url('cth1.png'), style={'height':'10%', 'width':'10%'}),
+           html.H4('Design OJK Coincidence Index 3.0'),
+           html.Img(src=app.get_asset_url('cth2.png'), style={'height':'10%', 'width':'10%'}),
             
            html.P('Paper Penyusunan OJK Index'),
            html.Br(),
-           html.P('Brownlees, C. dan R. Engle (2011), Volatility, Correlation, and Tails for Systemic Risk Measurement, Working Paper.'),
-           html.P('Engle, Robert (2002), Dynamic Conditional Correlation: A Simple Class of Multivariate GARCH Models, Journal of Business and Economic Statistics.'),
-           html.P('Hollo, D., Kremer, M., dan M. Lo Duca (2012), “CISS – A Composite Indicator of Systemic Stress in the Financial System, Working Paper Series No. 1426, Macroprudential Research Network (MARS)’),
-           html.P('Lo Duca, Marco dan Tuomas A, Petone (2011), Macro-financial Vulnerabilities and Future Financial Stress: Assessing Systemic Risks and Predicting Systemic Events, Working Paper Series No. 1311, Macroprudential Research Network (MARS), Maret'), 
-
-            
-
+           
 
            
             
